a6374bbc18f19b171f7a2613f7223f63c36662d9
mrAppleXZ <mr.applexz@gmail.com>
2022-01-30 19:43:41 +0500

    Update CIFAR example

--- example/cifar10.py
+++ example/cifar10.py
@@ -13,7 +13,9 @@ from torchvision.transforms import ToTensor
 from xztrainer import XZTrainer, XZTrainerConfig, SchedulerType, SavePolicy, XZTrainable, BaseContext, DataType, \
     ModelOutputType, ClassifierType
 from xztrainer.engine.standard import StandardEngineConfig
+from xztrainer.logger.compose import ComposeLoggingEngineConfig
 from xztrainer.logger.stream import StreamLoggingEngineConfig
+from xztrainer.logger.tensorboard import TensorboardLoggingEngineConfig
 
 if __name__ == '__main__':
     dataset_train = CIFAR10(root='./cifar10', download=True, train=True, transform=ToTensor())
@@ -42,9 +44,9 @@ if __name__ == '__main__':
 
     trainer = XZTrainer(
         config=XZTrainerConfig(
-            batch_size=8,
-            batch_size_eval=32,
-            epochs=5,
+            batch_size=128,
+            batch_size_eval=256,
+            epochs=10,
             optimizer=lambda module: AdamW(module.parameters(), lr=1e-3, weight_decay=1e-4),
             engine=StandardEngineConfig(),
             experiment_name='cifar10',
@@ -56,7 +58,7 @@ if __name__ == '__main__':
             accumulation_batches=4,
             print_steps=10,
             save_policy=SavePolicy.EVERY_EPOCH,
-            logger=StreamLoggingEngineConfig()
+            logger=ComposeLoggingEngineConfig(TensorboardLoggingEngineConfig(), StreamLoggingEngineConfig())
         ),
         model=resnet18(pretrained=False, num_classes=10),
         trainable=SimpleTrainable()
